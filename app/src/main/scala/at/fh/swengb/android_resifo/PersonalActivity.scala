6e8c70a2446b4fd18a95ebde4d6a13a2e3ab4071
kiv1995 <kevin.stessel@edu.fh-joanneum.at>
2017-01-17 08:52:27 +0100

    Test der Datenbank

--- app/src/main/scala/at/fh/swengb/android_resifo/PersonalActivity.scala
+++ app/src/main/scala/at/fh/swengb/android_resifo/PersonalActivity.scala
@@ -1,16 +1,22 @@
 package at.fh.swengb.android_resifo
 
-import android.content.Intent
+import android.app.Activity
+import android.content.{ContentValues, Context, Intent}
+import android.database.Cursor
+import android.database.sqlite.{SQLiteDatabase, SQLiteOpenHelper}
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
 import android.view.View
+import android.widget.EditText
 import at.fh.swengb.resifo_android.{MainActivity, R}
 
-
 class PersonalActivity extends AppCompatActivity{
-  override protected def onCreate(savedInstanceState: Bundle): Unit = {
+  var importData: DataBase = _
+  override protected def onCreate(savedInstanceState: Bundle){
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_personal)
+    importData = DataBase(getApplicationContext)
   }
   def GoBackHomeScreen(view: View):Unit={
     val homeBack = new Intent(this,classOf[MainActivity])
@@ -19,5 +25,35 @@ class PersonalActivity extends AppCompatActivity{
   def GoToAnmeldung(view: View):Unit={
     val nextPage = new Intent(this,classOf[AnmeldungActivity])
     startActivity(nextPage)
+  }
+
+
+  def saveToDb(view: View): Unit = {
+    val firstNameEditText = findViewById(R.id.vorname).asInstanceOf[EditText]
+    val firstName: String = firstNameEditText.getText.toString
+    println(firstName)
+    val cv = new ContentValues()
+    Map("firstname" -> firstName) foreach
+      {
+        case (k, v) => cv.put(k, v)
+      }
+    importData.getWritableDatabase().insert("person", null, cv)
+
+    var someCursor: Option[Cursor] = None
+    try {
+      someCursor = Option(importData.getReadableDatabase.query("person", Array("id", "firstname"), null, null, null, null, null))
+      someCursor match {
+        case None => System.err.println("Could not execute query due to some reason")
+        case Some(c) =>
+          while (c.moveToNext()) {
+            val id = c.getInt(c.getColumnIndex("id"))
+            val firstName = c.getString(c.getColumnIndex("firstname"))
+            println(s"ID($id) : $firstName")
+          }
+      }
+    }
+    finally {
+      someCursor foreach (_.close())
+    }
   }
 }
